981c0ccade39ef7d69d82c35d93ff474486e19ab
Ryan Luong <lumintriet@gmail.com>
2026-04-20 09:31:58 +0700

    feat(playground): execute JS in the sandbox.

--- scripts/playground/src/index.ts
+++ scripts/playground/src/index.ts
@@ -2,17 +2,26 @@ import { Sandbox } from "@vercel/sandbox";
 
 import { envVars } from "@/lib/env-vars";
 
+const script = `const x = 10;
+const y = 20;
+console.log(\`Sum is: \${x + y}\`);`;
+
 async function main() {
+  console.log("Creating the sandbox...");
   const sandbox = await Sandbox.create({
     teamId: envVars.VERCEL_TEAM_ID,
     projectId: envVars.VERCEL_PROJECT_ID,
     token: envVars.VERCEL_OIDC_TOKEN,
+    runtime: "node24",
   });
+  console.log("Sandbox created.");
 
-  const result = await sandbox.runCommand("echo", [
-    "Hello from Vercel Sandbox!",
-  ]);
+  console.log("Running command...");
+  const result = await sandbox.runCommand("node", ["-e", script]);
+  console.log("Command finished. Result:");
   console.log(await result.stdout());
+
+  await sandbox.stop();
 }
 
 main().catch(console.error);
