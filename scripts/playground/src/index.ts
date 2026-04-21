108851e477ebd36e36dcf0f35285148457a6e241
Ryan Luong <lumintriet@gmail.com>
2026-04-21 09:58:43 +0700

    feat(playground): implement the loop with math problems.

--- scripts/playground/src/index.ts
+++ scripts/playground/src/index.ts
@@ -2,6 +2,9 @@ import { Sandbox } from "@vercel/sandbox";
 import { generateText } from "ai";
 import { join } from "node:path";
 
+import initialPrompt from "@/resources/prompts/initial.txt";
+import systemPrompt from "@/resources/prompts/system.txt";
+import mathProblems from "@/resources/math-problems.txt";
 import { envVars } from "@/lib/env-vars";
 import { LOGS_DIR } from "@/constants";
 import { groq } from "@/lib/groq";
@@ -22,13 +25,25 @@ async function main() {
   });
   console.log("Sandbox created.");
 
-  const messages: Message[] = [];
+  console.log("Creating math directory...");
+  await sandbox.mkDir("math");
+  console.log("Math director created.");
+
+  console.log("Writting problems.txt to math directory...");
+  await sandbox.writeFiles([
+    { path: "math/problems.txt", content: Buffer.from(mathProblems) },
+  ]);
+  console.log("Written problems.txt to math directory.");
+
+  const messages: Message[] = [{ role: "user", content: initialPrompt }];
 
   console.log("Begin agent's loop.");
-  for (let turn = 1; turn <= 10 && sandbox.status === "running"; turn++) {
-    console.log(`Turn ${turn}...`);
+  for (let turn = 1; turn <= 25 && sandbox.status === "running"; turn++) {
+    console.log(`Turn ${turn}:`);
+
     const { text } = await generateText({
       model: groq("openai/gpt-oss-120b"),
+      system: systemPrompt,
       messages,
     });
     messages.push({ role: "assistant", content: text });
@@ -36,11 +51,28 @@ async function main() {
     const result = await sandbox.runCommand("node", ["-e", text]);
     const output = await result.output("both");
     messages.push({ role: "user", content: output });
+
+    console.log("Checking for solutions...");
+    const stream = await sandbox.readFile({
+      path: "solutions.json",
+      cwd: "/vercel/sandbox/math",
+    });
+
+    if (stream === null) {
+      console.log("No solutions found. Agent's loop continues...");
+    } else {
+      console.log("Solutions found:");
+      const solutions = await new Response(stream).json();
+      console.log(solutions);
+      break;
+    }
   }
   console.log("Agent's loop ended.");
 
-  await sandbox.stop();
-  console.log("Sandbox stopped.");
+  if (sandbox.status !== "stopped") {
+    await sandbox.stop();
+    console.log("Sandbox stopped.");
+  }
 
   const logContent = JSON.stringify(messages, null, 2);
   const logFilePath = join(LOGS_DIR, "messages.json");
