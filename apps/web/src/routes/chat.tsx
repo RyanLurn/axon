ced213b3efd2b25446d768dcf69847a7fef35e07
Ryan Luong <lumintriet@gmail.com>
2026-04-22 18:01:36 +0700

    feat(web/chat): list messages on route load.

--- apps/web/src/routes/chat.tsx
+++ apps/web/src/routes/chat.tsx
@@ -1,9 +1,20 @@
 import { createFileRoute } from "@tanstack/react-router";
 
+import { listMessages } from "@/features/chat/list-messages.function";
+
 export const Route = createFileRoute("/chat")({
+  loader: () => listMessages(),
   component: ChatPage,
 });
 
 function ChatPage() {
-  return <div>Hello "/chat"!</div>;
+  const messages = Route.useLoaderData();
+
+  return (
+    <div className="flex flex-col gap-y-4">
+      {messages.map((message) => (
+        <p key={message.id}>{message.parts[0]?.text}</p>
+      ))}
+    </div>
+  );
 }
