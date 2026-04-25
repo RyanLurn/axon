a5677d3ac60fce77adadddf5878a7424d9d66c56
Ryan Luong <lumintriet@gmail.com>
2026-04-25 21:09:53 +0700

    feat(api/messages): define POST /messages route.

--- apps/api/src/index.ts
+++ apps/api/src/index.ts
@@ -1,6 +1,7 @@
+import { validator } from "hono/validator";
 import { Hono } from "hono";
 
-import { MessageSchema } from "@/schemas";
+import { MessageSchema, type Message } from "@/schemas";
 import { mockDb } from "@/mock-db";
 
 export const app = new Hono()
@@ -22,4 +23,26 @@ export const app = new Hono()
       return c.json({ error: "Message not found" }, 404);
     }
     return c.json({ data: foundMessage });
-  });
+  })
+  .post(
+    "/messages",
+    validator("json", (value, c) => {
+      const parseResult = MessageSchema.omit({ id: true }).safeParse(value);
+      if (parseResult.success === false) {
+        return c.json({ error: "Invalid message" });
+      }
+      return parseResult.data;
+    }),
+    (c) => {
+      const { role, content } = c.req.valid("json");
+      const newMessage: Message = {
+        id: crypto.randomUUID(),
+        role,
+        content,
+      };
+
+      mockDb.messages.push(newMessage);
+
+      return c.json({ data: newMessage }, 201);
+    }
+  );
