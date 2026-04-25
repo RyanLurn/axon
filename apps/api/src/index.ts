5b2d720a54ad7e06f12f1cdb870ce17b696ddbf9
Ryan Luong <lumintriet@gmail.com>
2026-04-25 20:53:29 +0700

    feat(api/messages): validate id path param.

--- apps/api/src/index.ts
+++ apps/api/src/index.ts
@@ -1,14 +1,23 @@
 import { Hono } from "hono";
 
+import { MessageSchema } from "@/schemas";
 import { mockDb } from "@/mock-db";
 
 export const app = new Hono()
   .get("/", (c) => c.text("OK"))
   .get("/messages", (c) => c.json({ data: mockDb.messages }))
   .get("/messages/:id", (c) => {
+    // Parse the id path param
     const id = c.req.param("id");
-    const foundMessage = mockDb.messages.find((message) => message.id === id);
+    const parseIdResult = MessageSchema.shape.id.safeParse(id);
+    if (parseIdResult.success === false) {
+      return c.json({ error: "Invalid message id" }, 400);
+    }
 
+    // Find the requested message
+    const foundMessage = mockDb.messages.find(
+      (message) => message.id === parseIdResult.data
+    );
     if (foundMessage === undefined) {
       return c.json({ error: "Message not found" }, 404);
     }
