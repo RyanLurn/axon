1d239931eb712eb548e4a967693d7bf6ea9f3e1a
Ryan Luong <lumintriet@gmail.com>
2026-04-25 20:14:39 +0700

    feat(api): make use of the port constant from core package.

--- apps/api/src/serve.ts
+++ apps/api/src/serve.ts
@@ -1,3 +1,10 @@
+import { API_APP_PORT } from "@repo/core/constants/ports";
+
 import { app } from "@/index";
 
-export default app;
+const server = Bun.serve({
+  port: API_APP_PORT,
+  fetch: app.fetch,
+});
+
+console.log(`Server running at ${server.url.href}`);
