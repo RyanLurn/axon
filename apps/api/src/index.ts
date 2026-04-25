bf8d34650bfb465163f74c518d1ab6e26ae2799a
Ryan Luong <lumintriet@gmail.com>
2026-04-25 21:22:05 +0700

    feat(api/messages): return more helpful errors.

--- apps/api/src/index.ts
+++ apps/api/src/index.ts
@@ -1,4 +1,5 @@
 import { validator } from "hono/validator";
+import { prettifyError } from "zod";
 import { Hono } from "hono";
 
 import { MessageSchema, type Message } from "@/schemas";
@@ -7,29 +8,48 @@ import { mockDb } from "@/mock-db";
 export const app = new Hono()
   .get("/", (c) => c.text("OK"))
   .get("/messages", (c) => c.json({ data: mockDb.messages }))
-  .get("/messages/:id", (c) => {
-    // Parse the id path param
-    const id = c.req.param("id");
-    const parseIdResult = MessageSchema.shape.id.safeParse(id);
-    if (parseIdResult.success === false) {
-      return c.json({ error: "Invalid message id" }, 400);
+  .get(
+    "/messages/:id",
+    validator("param", (value, c) => {
+      const parseResult = MessageSchema.shape.id.safeParse(value.id);
+      if (parseResult.success === false) {
+        return c.json(
+          {
+            error: {
+              code: "VALIDATION_ERROR",
+              message: prettifyError(parseResult.error),
+            },
+          },
+          400
+        );
+      }
+      return parseResult.data;
+    }),
+    (c) => {
+      const id = c.req.valid("param");
+      const foundMessage = mockDb.messages.find((message) => message.id === id);
+      if (foundMessage === undefined) {
+        return c.json(
+          {
+            error: { code: "NOT_FOUND_ERROR", message: "Message not found" },
+          },
+          404
+        );
+      }
+      return c.json({ data: foundMessage });
     }
-
-    // Find the requested message
-    const foundMessage = mockDb.messages.find(
-      (message) => message.id === parseIdResult.data
-    );
-    if (foundMessage === undefined) {
-      return c.json({ error: "Message not found" }, 404);
-    }
-    return c.json({ data: foundMessage });
-  })
+  )
   .post(
     "/messages",
     validator("json", (value, c) => {
       const parseResult = MessageSchema.omit({ id: true }).safeParse(value);
       if (parseResult.success === false) {
-        return c.json({ error: "Invalid message" });
+        return c.json({
+          error: {
+            code: "VALIDATION_ERROR",
+            message: prettifyError(parseResult.error),
+          },
+        });
       }
       return parseResult.data;
     }),
