fdcaf7d2323ffee4a8a8984755ebead075cf4dc4
Ryan Luong <lumintriet@gmail.com>
2026-04-25 20:44:57 +0700

    feat(api/messages): define GET /messages/:id route.

--- apps/api/src/index.ts
+++ apps/api/src/index.ts
@@ -6,4 +6,13 @@ export const app = new Hono()
   .get("/", (c) => c.text("OK"))
   .get("/messages", (c) => {
     return c.json({ data: mockDb.messages });
+  })
+  .get("/messages/:id", (c) => {
+    const id = c.req.param("id");
+    const foundMessage = mockDb.messages.find((message) => message.id === id);
+
+    if (foundMessage === undefined) {
+      return c.json({ error: "Message not found" }, 404);
+    }
+    return c.json({ data: foundMessage });
   });
