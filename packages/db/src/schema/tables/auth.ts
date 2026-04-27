4ab6f10d6dc271c16e5fddb3dfbda927fc52d2e5
Ryan Luong <lumintriet@gmail.com>
2026-04-27 09:14:24 +0700

    feat(db/schema/auth): create verificationTable.

--- packages/db/src/schema/tables/auth.ts
+++ packages/db/src/schema/tables/auth.ts
@@ -39,3 +39,15 @@ export const accountTable = pgTable(
   },
   (table) => [index("accounts_user_id_idx").on(table.userId)]
 );
+
+export const verificationTable = pgTable(
+  "verifications",
+  {
+    id,
+    identifier: text("identifier").notNull(),
+    value: text("value").notNull(),
+    expiresAt: timestamp("expires_at", timestampConfig).notNull(),
+    ...timestamps,
+  },
+  (table) => [index("verifications_identifier_idx").on(table.identifier)]
+);
