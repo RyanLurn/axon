65622bac32c2550d74d6445f4c06e5bd6bfa71f9
Ryan Luong <lumintriet@gmail.com>
2026-04-27 17:06:05 +0700

    fix(randomizer): remove unnecessary code.

--- packages/randomizer/src/index.ts
+++ packages/randomizer/src/index.ts
@@ -1,14 +1,11 @@
-import { femaleFirstNames, maleFirstNames } from "@/datasets/names/first";
 import { capitalizeFirstLetter, getRandomItem } from "@/utils";
+import { firstNames } from "@/datasets/names/first";
 import { adjectives } from "@/datasets/adjectives";
 import { lastNames } from "@/datasets/names/last";
 import { nouns } from "@/datasets/nouns";
 
 export function generateNickname() {
-  const gender = Math.random() > 0.5 ? "male" : "female";
-  const randomFirstName = getRandomItem(
-    gender === "male" ? maleFirstNames : femaleFirstNames
-  );
+  const randomFirstName = getRandomItem(firstNames);
   const randomLastName = getRandomItem(lastNames);
   const randomAdjective = capitalizeFirstLetter(getRandomItem(adjectives));
   const randomNoun = capitalizeFirstLetter(getRandomItem(nouns));
