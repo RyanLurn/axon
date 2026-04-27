e530ea98737a6b0b108c940c823d0ade06c6900e
Ryan Luong <lumintriet@gmail.com>
2026-04-27 16:47:22 +0700

    feat(email/ethereal): define getEtherealTransport function.

---
+++ packages/email/src/adapters/ethereal/get-transport.ts
@@ -0,0 +1,27 @@
+import {
+  createTestAccount,
+  type Transporter,
+  createTransport,
+} from "nodemailer";
+
+let cachedTransporter: Transporter | undefined;
+
+export async function getEtherealTransport() {
+  if (cachedTransporter) {
+    return cachedTransporter;
+  }
+
+  const testAccount = await createTestAccount();
+  const transporter = createTransport({
+    auth: {
+      user: testAccount.user,
+      pass: testAccount.pass,
+    },
+    host: "smtp.ethereal.email",
+    secure: false,
+    port: 587,
+  });
+
+  cachedTransporter = transporter;
+  return cachedTransporter;
+}
