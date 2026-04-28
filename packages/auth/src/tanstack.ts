0c0d530819c752ef396e9cfa64d8f0811c763e8d
Ryan Luong <lumintriet@gmail.com>
2026-04-28 09:27:33 +0700

    feat(auth/tanstack): add apiKey plugin.

--- packages/auth/src/tanstack.ts
+++ packages/auth/src/tanstack.ts
@@ -1,9 +1,11 @@
 import { tanstackStartCookies } from "better-auth/tanstack-start";
+import { apiKey } from "@better-auth/api-key";
 import { betterAuth } from "better-auth";
 
 import { baseOptions } from "@/options/base";
 
 export const auth = betterAuth({
   ...baseOptions,
-  plugins: [tanstackStartCookies()], // make sure tanstackStartCookies is the last plugin in the array
+  // make sure tanstackStartCookies is the last plugin in the array
+  plugins: [apiKey(), tanstackStartCookies()],
 });
