f811561710e81cba4ba9ca970f208f64dca24717
Ryan Luong <lumintriet@gmail.com>
2026-05-05 08:39:29 +0700

    feat(db/schema/tables): modify users table schema.

--- packages/db/src/schema/tables/user.ts
+++ packages/db/src/schema/tables/user.ts
@@ -1,4 +1,4 @@
-import { boolean, pgTable, text } from "drizzle-orm/pg-core";
+import { boolean, pgTable, index, text } from "drizzle-orm/pg-core";
 import { generateNickname } from "@repo/randomizer";
 
 import type { UserId } from "@/types";
@@ -6,14 +6,18 @@ import type { UserId } from "@/types";
 import { timestampsWithDelete } from "@/schema/helpers/timestamps";
 import { id } from "@/schema/helpers/id";
 
-export const userTable = pgTable("users", {
-  id: id.$type<UserId>(),
-  name: text("name").notNull(),
-  nickname: text("nickname")
-    .notNull()
-    .$default(() => generateNickname()),
-  email: text("email").notNull().unique(),
-  emailVerified: boolean("email_verified").notNull(),
-  image: text("image"),
-  ...timestampsWithDelete,
-});
+export const userTable = pgTable(
+  "users",
+  {
+    id: id.$type<UserId>(),
+    name: text("name"),
+    nickname: text("nickname")
+      .notNull()
+      .$default(() => generateNickname()),
+    email: text("email").unique(),
+    emailVerified: boolean("email_verified").notNull(),
+    image: text("image"),
+    ...timestampsWithDelete,
+  },
+  (table) => [index("nickname_idx").on(table.nickname)]
+);
