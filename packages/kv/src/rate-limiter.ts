8429e082aacc608a09f44f83399e10553f5f16c5
Ryan Luong <lumintriet@gmail.com>
2026-05-05 08:28:39 +0700

    feat(kv/rate-limit): create rateLimiter instance.

---
+++ packages/kv/src/rate-limiter.ts
@@ -0,0 +1,10 @@
+import { Ratelimit } from "@upstash/ratelimit";
+
+import { kv } from "@/index";
+
+export const rateLimiter = new Ratelimit({
+  redis: kv,
+  limiter: Ratelimit.tokenBucket(5, "10 s", 10),
+  analytics: true,
+  prefix: "axon-rate-limiter",
+});
